4c2ebb75cab106a90a81abc6d069ce743da7c63e
rocketfish <rocket.fish@mail.com>
2021-03-02 19:32:12 -0500

    remove api suffix

--- start/routes.ts
+++ start/routes.ts
@@ -48,7 +48,4 @@ Route.group(() => {
     const report = await HealthCheck.getReport();
     return report.healthy ? response.ok(report) : response.badRequest(report);
   });
-}).prefix("/api");
-Route.get("*", async () => {
-  return { hello: "world" };
 });
